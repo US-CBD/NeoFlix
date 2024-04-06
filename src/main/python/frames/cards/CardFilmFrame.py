65ee9e31d02dbd9dd740117a81c6c4d94519bb45
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-04-06 13:15:21 +0200

    refactor: Card for film and person

---
+++ src/main/python/frames/cards/CardFilmFrame.py
@@ -0,0 +1,39 @@
+import os
+import tkinter as tk
+
+import customtkinter as ctk
+from PIL import Image
+
+try:
+    from src.main.python.frames.details.DetailsFilmFrame import DetailsFilmFrame
+except ModuleNotFoundError:
+    from frames.details.DetailsFilmFrame import DetailsFilmFrame
+
+
+class CardFilmFrame:
+    def __init__(self, parent_frame, film , row, column, size):
+        self.parent_frame = parent_frame
+        self.film = film
+        self.row = row
+        self.column = column
+        self.size = size
+        self.initialize()
+
+    def initialize(self):
+        name = ctk.CTkLabel(self.parent_frame, text=self.film.title, fg_color="gray30", corner_radius=6)
+        name.grid(row=self.row, column=self.column)
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.film.image)
+        image = ctk.CTkImage(Image.open(image_path), size=self.size)
+        button = ctk.CTkButton(self.parent_frame, text="", image=image, command=lambda: self.show_detail_frame())
+        button.grid(row=self.row + 1, column=self.column)
+
+    def show_detail_frame(self):
+        self.details_window = tk.Toplevel(self)
+        self.details_window.configure(bg="black")
+
+        # Create an instance of DetailsFilmFrame with the film data and pack it into the new window
+        self.detail_frame = DetailsFilmFrame(self.details_window, self.film)
+        self.detail_frame.pack()
+
+        # Set the title of the details window
+        self.details_window.title(self.film.title)
---
+++ src/main/python/frames/cards/CardPersonFrame.py
@@ -0,0 +1,38 @@
+import os
+import tkinter as tk
+
+import customtkinter as ctk
+from PIL import Image
+
+try:
+    from src.main.python.frames.details.DetailsPersonFrame import DetailsPersonFrame
+except ModuleNotFoundError:
+    from frames.details.DetailsPersonFrame import DetailsPersonFrame
+
+
+class CardPersonFrame:
+    def __init__(self, parent_frame, person, row, column, size):
+        self.parent_frame = parent_frame
+        self.person = person
+        self.row = row
+        self.column = column
+        self.size = size
+
+    def initialize(self):
+        name = ctk.CTkLabel(self.parent_frame, text=self.person.name, fg_color="gray30", corner_radius=6)
+        name.grid(row=self.row, column=self.column)
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.person.image)
+        image = ctk.CTkImage(Image.open(image_path), size=self.size)
+        button = ctk.CTkButton(self.parent_frame, text="", image=image, command=lambda: self.show_detail_frame())
+        button.grid(row=self.row+1, column=self.column)
+
+    def show_detail_frame(self):
+        self.details_window = tk.Toplevel(self)
+        self.details_window.configure(bg="black")
+
+        # Create an instance of DetailsFilmFrame with the film data and pack it into the new window
+        self.detail_frame = DetailsPersonFrame(self.details_window, self.person)
+        self.detail_frame.pack()
+
+        # Set the title of the details window
+        self.details_window.title(self.person.name)
